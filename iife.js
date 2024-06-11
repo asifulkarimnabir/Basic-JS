88d1c913801764bbeb5d1d6f4de4e40ead5ad9f2
asifulkarimnabir <asifulkarimnabir17@gmail.com>
2024-06-11 21:47:04 +0600

    IIFE in js

---
+++ iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// function myFunction() {
+//   console.log("Nabir");
+// }
+// myFunction();
+
+(function myFunction() {
+  console.log("Nabir");
+})();
+
+(() => {
+  console.log("Nabir");
+})();
